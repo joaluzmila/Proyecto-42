182c26466d103fe65f8fd08f0599b3086eb46cf0
joaluzmila <86448563+joaluzmila@users.noreply.github.com>
2021-12-29 13:12:58 -0600

    Add files via upload

---
+++ Drop.js
@@ -0,0 +1,40 @@
+class Drop{
+	constructor(x,y,r)
+	{
+		var options={
+			'restitution':0.1,
+            'friction':0.1,
+            'density':1.0
+           
+		}
+		this.x=x;
+		this.y=y;
+		this.body = Bodies.circle(x,y,5,options)
+        this.radius = 5;
+        World.add(world, this.body)
+
+	}
+	updateY(){
+		if(this.body.position.y > 700){
+
+			Matter.Body.setPosition(this.body, {x:random(0,400), y:random(0,400)})
+		}
+	}
+	display()
+	{
+			var droppos=this.body.position;		
+			push()
+			translate(droppos.x, droppos.y);
+			rectMode(CENTER)
+			fill(rgb(92, 114, 237));
+            ellipseMode(CENTER);
+            ellipse(this.body.position.x,this.body.position.y,this.radius,this.radius);
+            pop()
+	}
+
+
+	
+
+    
+
+}
